dcc73d57b2d98fdc793a8a2cf9460b3f6834841d
jrandiny <jrandiny2@gmail.com>
2019-09-04 20:26:08 +0700

    Update database connection

---
+++ ormconfig.js
@@ -0,0 +1,36 @@
+function getConfig() {
+  let config = {
+    "synchronize": true,
+    "logging": false,
+    "entities": [
+      "src/entity/**/*.ts"
+    ],
+    "migrations": [
+      "src/migration/**/*.ts"
+    ],
+    "subscribers": [
+      "src/subscriber/**/*.ts"
+    ],
+    "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+    }
+  }
+  if (process.env.DB_URL) {
+    return {
+      "type": process.env.DB_TYPE,
+      "url": process.env.DB_URL,
+      ...config
+    }
+  } else {
+    return {
+      "type": "sqlite",
+      "database": "database.sqlite",
+      ...config
+    }
+  }
+}
+
+module.exports = getConfig();
+
