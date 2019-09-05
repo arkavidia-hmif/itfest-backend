58e3ded2ccbe73539265c9463443a4d115155847
jrandiny <jrandiny2@gmail.com>
2019-09-05 09:05:42 +0700

    Add simple CRUD controller

---
+++ src/controller/InventoryController.ts
@@ -0,0 +1,26 @@
+import { getRepository } from "typeorm";
+import { NextFunction, Request, Response } from "express";
+import { Inventory } from "../entity/Inventory";
+
+export class UserController {
+
+  private Inventory = getRepository(Inventory);
+
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.Inventory.find();
+  }
+
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.Inventory.findOne(request.params.id);
+  }
+
+  async save(request: Request, response: Response, next: NextFunction) {
+    return this.Inventory.save(request.body);
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    let userToRemove = await this.Inventory.findOne(request.params.id);
+    await this.Inventory.remove(userToRemove);
+  }
+
+}
---
+++ src/controller/ItemController.ts
@@ -0,0 +1,26 @@
+import { getRepository } from "typeorm";
+import { NextFunction, Request, Response } from "express";
+import { Item } from "../entity/Item";
+
+export class UserController {
+
+  private itemRepository = getRepository(Item);
+
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.itemRepository.find();
+  }
+
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.itemRepository.findOne(request.params.id);
+  }
+
+  async save(request: Request, response: Response, next: NextFunction) {
+    return this.itemRepository.save(request.body);
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    let userToRemove = await this.itemRepository.findOne(request.params.id);
+    await this.itemRepository.remove(userToRemove);
+  }
+
+}
