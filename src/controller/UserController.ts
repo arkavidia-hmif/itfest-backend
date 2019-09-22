69a9dff2b1f4fc6cf220ca37d23fb9eefb810874
jrandiny <jrandiny2@gmail.com>
2019-09-22 17:44:39 +0700

    Implement login

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,62 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import { getRepository } from "typeorm";
+import { NextFunction, Request, Response } from "express";
+import { User, UserRole } from "../entity/User";
+import * as  bcrypt from "bcrypt";
+
+import config from "../config";
 
 export class UserController {
 
-    private userRepository = getRepository(User);
+  private userRepository = getRepository(User);
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+  async createUser(name: string, username: string, email: string, role: UserRole, password: string) {
+    const salt = bcrypt.genSaltSync(config.password.saltRounds);
+
+    const encryptedHash = bcrypt.hashSync(password, salt);
+
+    await this.userRepository.save({
+      name,
+      username,
+      role,
+      email,
+      salt,
+      password: encryptedHash,
+    });
+  }
+
+  async login(request: Request, response: Response, next: NextFunction) {
+    const { username, email, password, pin } = request.body;
+
+    const userByUsername = await this.userRepository.findOne({
+      username
+    });
+
+    const userByEmail = await this.userRepository.findOne({
+      email
+    });
+
+    const user = userByEmail || userByUsername;
+
+    if (!user) {
+      return response.status(404).json({
+        status: 404,
+        code: "user-not-found"
+      });
     }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
+    if (bcrypt.compareSync(password, user.password)) {
+      return response.status(200).json({
+        status: 200,
+        code: "ok",
+        data: {
+          jwt: ""
+        }
+      });
+    } else {
+      return response.status(401).json({
+        status: 401,
+        code: "invalid-auth"
+      });
     }
-
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
-    }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
-    }
-
+  }
 }
