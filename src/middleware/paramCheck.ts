41dfbdfbb416ce4267d59740fa972c2cbb8f9061
jrandiny <jrandiny@gmail.com>
2020-01-15 18:23:39 +0700

    Add parameter checker middleware

---
+++ src/middleware/paramCheck.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { responseGenerator } from "../utils/responseGenerator";
+
+export function paramCheck(request: Request, response: Response, next: NextFunction) {
+  const errors = validationResult(request);
+  if (errors.isEmpty()) {
+    return next();
+  } else {
+    const errorArray = errors.array();
+
+    const errorResult = errorArray.map((data) => {
+      return {
+        "part": data.param,
+        "message": data.msg
+      };
+    });
+
+    return responseGenerator(response, 400, "invalid-input", errorResult);
+  }
+}
