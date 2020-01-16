57df3cb05e19649f8f375781ae2e9730c5145053
jrandiny <jrandiny@gmail.com>
2020-01-16 10:18:32 +0700

    Add validation to common user route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,9 @@ import { UserController } from "../controller/UserController";
 import { UserRole } from "../entity/User";
 import { checkJWT } from "../middleware/checkJWT";
 import { limitAccess } from "../middleware/limitAccess";
+import { check, oneOf, param } from "express-validator";
+import { paramCheck } from "../middleware/paramCheck";
+import { paginationCheck } from "../middleware/paginationCheck";
 
 export default () => {
   const router = Router();
@@ -11,15 +14,57 @@ export default () => {
   const uc = new UserController();
   const tc = new TransactionController();
 
-  router.post("/login", uc.login.bind(uc));
+  router.post("/login", [
+    oneOf([
+      check("email")
+        .isEmail().withMessage("must be a valid email address"),
+      check("username")
+        .isAlphanumeric().withMessage("must be an alphanumeric sequence")
+    ]),
+    check("password")
+      .not().isEmpty().withMessage("must be provided"),
+    paramCheck
+  ], uc.login.bind(uc));
+
+  router.post("/activate", [
+    check("email")
+      .isEmail().withMessage("must be a valid email address"),
+    check("password")
+      .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{0,}$/, "i")
+      .withMessage("must include one lowercase character, one uppercase character, a number, and a special character")
+      .isLength({ "min": 8 }).withMessage("must be at least 8 characters long"),
+    check("voucher")
+      .isAlphanumeric().withMessage("must be alphanumeric")
+      .isLength({ "min": 6, "max": 6 }).withMessage("must be 6 characters long"),
+    check("name")
+      .optional()
+      .isAlpha().withMessage("must only contain letter"),
+    check("gender")
+      .optional()
+      .isInt({ "min": 1, "max": 2 }).withMessage("must be a valid gender (1=male, 2=female)"),
+    check("interest")
+      .optional()
+      .isArray().withMessage("must be an array"),
+    check("dob")
+      .optional()
+      .isISO8601().withMessage("must be a valid ISO8601 date"),
+    paramCheck
+  ], uc.registerVisitor.bind(uc));
 
   router.use("/user", checkJWT);
   router.get("/user/me", uc.getMe.bind(uc));
   router.get("/user/me/transaction", uc.getMeTransaction.bind(uc));
-  router.get("/user/:id", limitAccess([UserRole.ADMIN]), uc.getUser.bind(uc));
-  router.get("/user/:id/transaction", limitAccess([UserRole.ADMIN]), uc.getTransaction.bind(uc));
+  router.get("/user/:id([0-9]+)", limitAccess([UserRole.ADMIN]), uc.getUser.bind(uc));
+  router.get("/user/:id([0-9]+)/transaction", [
+    limitAccess([UserRole.ADMIN]),
+    ...paginationCheck,
+    paramCheck
+  ], uc.getTransaction.bind(uc));
 
-  router.post("/user/:id/give", tc.give.bind(tc));
+  router.post("/user/:id([0-9]+)/give", [
+    check("amount").isInt({ min: 0 }),
+    paramCheck
+  ], tc.give.bind(tc));
 
   router.get("/test-jwt", [checkJWT], (req, res) => {
     res.json({
