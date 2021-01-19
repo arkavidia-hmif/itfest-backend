4d70501e5926f1360e87b83105600e55a78260ca
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-01-19 16:58:14 +0700

    revert setting db and env

--- ormconfig.js
+++ ormconfig.js
@@ -23,30 +23,16 @@ function getConfig() {
       "subscribersDir": "src/subscriber"
     }
   }
-
-  // if (process.env.DB_URL) {
-  //   return {
-  //     "type": process.env.DB_TYPE,
-  //     "database": process.env.DB_NAME,
-  //     "url": process.env.DB_URL,
-  //     ...config
-  //   }
-  // } else {
-  //   console.error("Please specify database (check Readme)");
-  //   process.exit(1);
-  // }
-
-  return {
-    "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    // "username": "debian-sys-maint",
-    // "password": "DAoicYZ8uK582Zjg",
-    "username": "root",
-    "password": "root",
-    "database": "itfest2",
-    "insecureAuth" : true,
-    ...config
+  if (process.env.DB_URL) {
+    return {
+      "type": process.env.DB_TYPE,
+      "database": process.env.DB_NAME,
+      "url": process.env.DB_URL,
+      ...config
+    }
+  } else {
+    console.error("Please specify database (check Readme)");
+    process.exit(1);
   }
 }
 
