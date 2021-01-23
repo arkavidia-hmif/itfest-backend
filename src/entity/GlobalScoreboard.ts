a9595eb44574271ff7685433d9c23bc1ea49393c
jrandiny <jrandiny@gmail.com>
2021-01-23 13:33:44 +0700

    Use UpdateDateColumn for GlobalScoreboard

--- src/entity/GlobalScoreboard.ts
+++ src/entity/GlobalScoreboard.ts
@@ -1,29 +1,22 @@
 import {
     Column,
-    CreateDateColumn,
     Entity,
-    PrimaryColumn,
     JoinColumn,
-    ManyToOne,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
-    OneToOne,
 } from "typeorm";
 import { User } from "./User";
 
 @Entity()
 export class GlobalScoreboard {
     @PrimaryGeneratedColumn()
-    @JoinColumn({ name : 'userId' })
-    userId : number;
+    @JoinColumn({ name: 'userId' })
+    userId: number;
 
-    // @ManyToOne((type) => User, (user) => user.id, { nullable: false, onDelete: 'CASCADE' })
-    // user: User;
+    @Column({ default: () => 0 })
+    score: number;
 
-    @Column({ default : () => 0 })
-    score : number;
-
-    @Column({type: "datetime", default: () => "CURRENT_TIMESTAMP"})
+    @UpdateDateColumn()
     lastUpdated: Date;
 }
 
