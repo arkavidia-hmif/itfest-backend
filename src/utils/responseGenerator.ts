e533655a70d53b14a8e6704d081548bccdb7b9a5
jrandiny <jrandiny@gmail.com>
2021-01-23 23:19:47 +0700

    Set response generator to generic function

--- src/utils/responseGenerator.ts
+++ src/utils/responseGenerator.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 
-export function responseGenerator(response: Response, status: number, code: string, data?: Record<string, unknown>): Response {
+export function responseGenerator<T>(response: Response, status: number, code: string, data?: T): Response {
   return response.status(status).json({
     status,
     code,
