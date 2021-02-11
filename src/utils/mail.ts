e18d0a804397dfb341f3aad4ab2e9e93b5c3fa92
jrandiny <jrandiny@gmail.com>
2021-02-11 13:38:29 +0700

    Add smtp name to prevent DoS false alarm on Google SMTP Relay

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -4,6 +4,8 @@ export const transporter = createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: !!process.env.SMTP_SECURE,
+  pool: true,
+  name: process.env.SMTP_NAME,
   auth: {
     user: process.env.SMTP_USERNAME,
     pass: process.env.SMTP_PASSWORD,
