66400aca90131f27e8734b050b2369a4ae0dee11
jrandiny <jrandiny@gmail.com>
2021-02-20 14:39:43 +0700

    Recreate migration

--- src/migration/1613806710190-init.ts
+++ src/migration/1613806710190-init.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class init1612440731364 implements MigrationInterface {
-    name = 'init1612440731364'
+export class init1613806710190 implements MigrationInterface {
+    name = 'init1613806710190'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "user_role_enum" AS ENUM('admin', 'tenant', 'visitor')`);
