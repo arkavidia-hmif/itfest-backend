86ec9957e5ae4e2e2fdef2b7a34734832ec1cf22
jrandiny <jrandiny@gmail.com>
2021-02-24 23:36:11 +0700

    Add param check for checkout api

--- src/routes/checkout.ts
+++ src/routes/checkout.ts
@@ -5,6 +5,7 @@ import config from "../config";
 import { CheckoutController } from "../controller/CheckoutController";
 import { UserRole } from "../entity/User";
 import { checkJWT } from "../middleware/checkJWT";
+import { checkParam } from "../middleware/checkParam";
 import { limitAccess } from "../middleware/limitAccess";
 
 export default () => {
@@ -12,11 +13,11 @@ export default () => {
 
   const cc = new CheckoutController();
 
-  // const nameCheck = () => check("name").isAscii().withMessage("must not contain special character");
-  // const tenantCheck = () => check("tenant").isInt({ min: 1 }).withMessage("must be valid id");
-  // const problemCheck = () => check("owner").isString().withMessage("must be json stringify");
-  // const answerCheck = () => check("answer").isString().withMessage("must be json stringify");
-  // const diffCheck = () => check("difficulty").isInt({ min: 1 }).withMessage("must be valid difficulty");
+  const lineCheck = () => check("lineContact").isAscii().withMessage("must be valid LINE id");
+  const waCheck = () => check("waContact").isString().isLength({ min: 10, max: 20 }).withMessage("must be >=10 and <=20 characters long");
+  const addressCheck = () => check("address").isString().withMessage("must be valid address");
+  const itemCheck = () => check("items").isArray().withMessage("items must be array");
+  const itemContainCheck = () => check("items.*").isInt().withMessage("must be item id");
 
   router.use(checkJWT);
 
@@ -25,7 +26,13 @@ export default () => {
   ], cc.getCheckout.bind(cc));
 
   router.post("/checkout", [
-    limitAccess([UserRole.VISITOR])
+    limitAccess([UserRole.VISITOR]),
+    lineCheck(),
+    waCheck(),
+    addressCheck().optional(),
+    itemCheck(),
+    itemContainCheck(),
+    checkParam,
   ], cc.createCheckout.bind(cc));
 
   return router;
